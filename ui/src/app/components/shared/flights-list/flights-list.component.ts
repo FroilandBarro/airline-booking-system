21198348330b9cbc647abad3bc942c466554be98
Roland Barro <rolandbarro@gmail.com>
2018-10-08 00:27:07 +0800

    Updated admin dashboard

--- ui/src/app/components/shared/flights-list/flights-list.component.ts
+++ ui/src/app/components/shared/flights-list/flights-list.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from '../../../services/sharedData.service';
 
 @Component({
   selector: 'app-flights-list',
   templateUrl: './flights-list.component.html',
-  styleUrls: ['./flights-list.component.css']
+  styleUrls: ['./flights-list.component.scss']
 })
 export class FlightsListComponent implements OnInit {
 
-  constructor() { }
+  flights: any;
+  constructor(
+    private sharedData: SharedDataService,
+  ) {
+    this.sharedData.flights.subscribe((flights: any) => {
+      if (flights && flights.length) {
+        this.flights = flights;
+        console.log(flights);
+      }
+    });
+  }
 
   ngOnInit() {
   }
--- ui/src/app/services/sharedData.service.ts
+++ ui/src/app/services/sharedData.service.ts
@@ -7,15 +7,21 @@ import { SystemUtils } from './system.utils.service';
 export class SharedDataService {
     constructor(
         private utils: SystemUtils,
-    ){
+    ) {
         this.setUserData(this.utils.retrieveItem('userData'));
     }
 
     private userData = new BehaviorSubject<object>({});
     currentUserData = this.userData.asObservable();
 
+    private _flights = new BehaviorSubject<object>({});
+    flights = this._flights.asObservable();
+
     setUserData(userData: object) {
         return this.userData.next(userData);
     }
 
+    setFlights(flights: object) {
+        return this._flights.next(flights);
+    }
 }
